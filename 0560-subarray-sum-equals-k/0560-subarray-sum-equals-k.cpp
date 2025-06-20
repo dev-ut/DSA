b9466edf37df63091b3402fcb3df2c6b90b45c0d
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-06-20 12:16:18 +0530

    Time: 37 ms (82.94%), Space: 45.4 MB (58.24%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        // create prefix array in palce
+        for(int i=1;i<n;i++)
+        {
+            nums[i]+=nums[i-1];
+        }
+        // create and loop up and aad the frequency if found
+        unordered_map<int,int> map;
+
+        int count=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==k)count++;   // if dierct k== index value 
+            int rem=nums[i]-k;    
+            if(map.find(rem)!=map.end()) count=count+map[rem];
+            
+            map[nums[i]]++;
+        }
+        return count;
+    }
+};
