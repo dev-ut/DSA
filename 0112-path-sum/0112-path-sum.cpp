e640d7d78ec6f9af57d73f50752836baf22a3bce
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-06-27 12:10:39 +0530

    Time: 0 ms (100%), Space: 21.5 MB (34.19%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+bool preorder(TreeNode* root, int targetSum)
+{
+    if(root == NULL) return false;
+    if(root->left == NULL && root->right == NULL) {
+         if (root->val == targetSum) return true;
+    }
+     
+    // Traverse left and right subtrees
+    bool leftPath = preorder(root->left, targetSum - root->val);
+    bool rightPath = preorder(root->right, targetSum - root->val);   // hmne yahan yeh variable isliye bnaya hai taki hamare paas ek record rhe path ka
+
+    // Return true if either left or right subtree has a path sum equal to targetSum
+    return leftPath || rightPath;
+}
+    bool hasPathSum(TreeNode* root, int targetSum) 
+    {
+        return preorder(root, targetSum);
+    }
+};
