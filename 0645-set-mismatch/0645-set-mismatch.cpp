3502442c09c9ee555d07a38670d632c0e38651b5
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-04 00:23:51 +0530

    Time: 0 ms (100%), Space: 25.2 MB (56.77%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,28 +2,57 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) 
     {
-        int n=nums.size();
-        unordered_map<int ,int> mp;
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
-        }
-         int missing =-1;
-         int duplicate=-1;
+    //     int n=nums.size();
+    //     unordered_map<int ,int> mp;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mp[nums[i]]++;
+    //     }
+    //      int missing =-1;
+    //      int duplicate=-1;
        
 
-       for(int i=1;i<=n;i++)
-       {
-          if (mp[i]==2)
-          {
-            duplicate=i;
-          }
-          else if(mp[i]==0)
-          {
-           missing=i;
-          }
-       }
-       return{duplicate,missing};
-       
+    //    for(int i=1;i<=n;i++)
+    //    {
+    //       if (mp[i]==2)
+    //       {
+    //         duplicate=i;
+    //       }
+    //       else if(mp[i]==0)
+    //       {
+    //        missing=i;
+    //       }
+    //    }
+    //    return{duplicate,missing};
+
+      int i = 0;
+    while (i < nums.size())
+    {
+        int ctindx = nums[i] - 1;
+
+        if (nums[i] != nums[ctindx])
+        {
+            swap(nums[i], nums[ctindx]);
+        }
+        else
+        {
+            i++;
+        }
     }
+
+    int duplicate = -1;
+    int missing = -1;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != i + 1)
+        {
+            duplicate = nums[i]; // duplicate value present
+            missing = i + 1;     // missing value
+        }
+    }
+
+    return {duplicate, missing};
+}
+
 };
