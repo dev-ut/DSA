9cc6522336bf3d5ed53b98960b01a00f5c283510
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-07 19:37:46 +0530

    Time: 3 ms (34.99%), Space: 63 MB (12.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int maxArea(vector<int>& height) 
     {
-        int lo=0;
-        int hi=height.size()-1;
-        int mxcapacity=INT_MIN;
-        while(lo<=hi)
-        {
-           mxcapacity=max(mxcapacity,(hi-lo)*min(height[lo],height[hi]));
-           if(height[lo]<=height[hi]) lo++; // coz humko left side m greatest to mil hi gya now humko rihjt side m greatest chaiye
-           else hi--;
-        }
-        return mxcapacity;
-
+       int i=0;
+       int j=height.size()-1;
+       int mxcap=INT_MIN;
+       while(i<=j)
+       {
+         mxcap=max(mxcap,min(height[i],height[j])*(j-i));
+         if(height[i]<=height[j]) i++;
+         else j--;
+       }
+     return mxcap;
     }
 };
