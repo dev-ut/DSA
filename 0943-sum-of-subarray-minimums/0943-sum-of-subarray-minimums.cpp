51fe91e1d34384c02f51fbded7adf82b243f046e
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-09 18:16:36 +0530

    Time: 15 ms (70.36%), Space: 47.1 MB (81.26%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
-   int sumSubarrayMins(vector<int>& arr) 
-{
-    int m = 1e9 + 7;
-    //just the pure monstcak logic 
-    // nselmnt from one point to left will be the smallest till that 
-    int n = arr.size();
-    
-    vector<int> nsi(n);
-    stack<int> st;
-
-    nsi[n - 1] = n;
-    st.push(n - 1);
-
-    for (int i = n - 2; i >= 0; i--) 
+    int sumSubarrayMins(vector<int>& arr) 
     {
-        while (st.size() > 0 && arr[st.top()] >= arr[i]) 
+        int n = arr.size();
+        int mod = 1e9 + 7;
+
+        // Next smaller index
+        vector<int> nsi(n, n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) 
         {
-            st.pop();
+            while (!st.empty() && arr[st.top()] > arr[i]) 
+            {
+                st.pop();
+            }
+            if (!st.empty()) nsi[i] = st.top();
+            st.push(i);
         }
-        if (st.size() == 0) nsi[i] = n;
-        else nsi[i] = st.top(); // dal do 
 
-        st.push(i);
-    }
-
-    // previos samllest index nikal liya
-    vector<int> psi(n);
-    while (!st.empty()) st.pop(); // stack ko clear kr do
-
-    psi[0] = -1;
-    st.push(0);
-
-    for (int i = 1; i < n; i++) 
-    {
-        while (st.size() > 0 && arr[st.top()] > arr[i]) 
+        // Previous smaller index
+        vector<int> psi(n, -1);
+        while (!st.empty()) st.pop();
+        for (int i = 0; i < n; i++) 
         {
-            st.pop();
+            while (!st.empty() && arr[st.top()] >= arr[i]) 
+            { // >= is important
+                st.pop();
+            }
+            if (!st.empty()) psi[i] = st.top();
+            st.push(i);
         }
-        if (st.size() == 0) psi[i] = -1;
-        else psi[i] = st.top(); // dal do 
 
-        st.push(i);
+        // Contribution calculation
+        long long total = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            long long left = i - psi[i];
+            long long right = nsi[i] - i;
+            total = (total + (1LL * arr[i] * left * right) % mod) % mod;
+        }
+
+        return (int)total;
     }
-
-    //now ab hum eak point se kitna expand kr skte hai left right on conatgoius basic ki wo us subarry ka samleeest elmnt rhe 
-    long long total = 0;
-    for (int i = 0; i < n; i++) 
-    {
-        long long left = i - psi[i];
-        long long right = nsi[i] - i;
-        total = (total + (arr[i] * left % m) * right % m) % m;
-    }
-
-    return total;
-}
-
 };
