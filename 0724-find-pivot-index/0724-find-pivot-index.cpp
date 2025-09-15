9ab38de04307a315686528a17e9dde20e1519295
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-15 19:52:12 +0530

    Time: 0 ms (100%), Space: 35.8 MB (54.5%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) 
     {
-        int n=nums.size();
-        //very simple iska prefix sum mnikalo prefix array
-        //aur suffix sum nikalo suffix aaray jo bhi elemt same ho whi pivot index hoga.
-        //prefix
-        int par[n];
-        int sar[n];
-        par[0]=nums[0];
-
-        for(int i=1;i<n;i++)
+      int n=nums.size();
+      // agr prefix and suffix sum nikalne ke baad jis index pr dono ki value same a rhi hogi whi hamari pivot index hogi 
+      int ps[n];
+      ps[0]=nums[0];
+      for(int i=1;i<n;i++)
+      {
+        ps[i]=ps[i-1]+nums[i];
+      } 
+      // suffix sum
+      int ss[n];
+      ss[n-1]=nums[n-1];
+      for(int i=n-2;i>=0;i--)
+      {
+        ss[i]=ss[i+1]+nums[i];
+      }  
+      for(int i=0;i<n;i++)
+      {
+        if(ss[i]==ps[i])
         {
-            par[i]=par[i-1]+nums[i];  // prefix aary taiyar hua
+            return i;
         }
-       
-        //suffix aary
-        sar[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            sar[i]=sar[i+1]+nums[i];
-        }
-                  // now comapring index values
-        for(int i=0;i<n;i++)
-        {
-            if(sar[i]==par[i]) return i;
-        }
-        return -1;
+      }
+      return -1;
     }
 };
