3f767c446e63d03b2adc6e0d0473489950adfedc
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-15 19:39:22 +0530

    Time: 3 ms (29.77%), Space: 22.2 MB (41.49%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,32 +3,34 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) 
     {
       int n=asteroids.size();
-      stack<int>st;   //[4,7,1,1,2,-3,-7,17,15,-16]
+      stack<int>st;
 
-      for(int i=0;i<n;i++)
+      for(int i=0;i<n;i++)  //[4,7,1,1,2,-3,-7,17,15,-16]
       {
-        if(asteroids[i]<0)  // agr negative no 3 conditions 
+        if(asteroids[i]<0) // ie agr negative elment hai to 
         {
-            while(st.size()>0 && st.top()>0 && abs(st.top())<abs(asteroids[i]))
+            while(st.size()>0 && st.top()>0 && abs(st.top())<abs(asteroids[i])) // we will pop the elements until 
             {
                 st.pop();
             }
-            // if the elmets are qual 
+            // if the mangintude of postive nad negative elment are equal pop and get indx+1
             if(st.size()>0 && st.top()>0 && abs(st.top())==abs(asteroids[i]))
             {
                 st.pop();
             }
-            else if(st.size()==0 || st.top() < 0)
-                {
-                    st.push(asteroids[i]);
-                }
+            else if(st.size()==0 || st.top()<0)
+            {
+                st.push(asteroids[i]);
+            }
         }
-        else
+        else  // if sare posistives elment hai to just push back  
         {
-            st.push(asteroids[i]);
+           st.push(asteroids[i]);
         }
       }
-      vector<int> ans(st.size());
+
+      // ab ise vecto mai wapas denge
+       vector<int> ans(st.size());
 
       for(int i=st.size()-1;i>=0;i--)
       {
@@ -36,8 +38,5 @@ public:
         st.pop();
       }
       return ans;
-
-      
-        
     }
 };
