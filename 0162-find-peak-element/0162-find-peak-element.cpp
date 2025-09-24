6c9e7b93b2e7cfaa86dc809a3611f181f81df4b8
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-24 21:17:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (32.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) 
-    {
-      int n=nums.size();
-      int lo=0;
-      int hi=nums.size()-1;
-      while(lo<=hi)
-      {
-       int mid=lo+(hi-lo)/2;
-       if(mid==0)   // if the array is sorted 4,3,2,1
-       {
-        if(nums[mid]>nums[mid+1])
-        {
-            return mid;
-        }
-        else
-        {
-          lo=mid+1;
-        }
-       }
-       if(mid==n-1)  // if the array is sorted 1,2,3,4
-       {
-        if(nums[mid]>nums[mid-1])
-        {
-            return mid;
-        }
-        else
-        {
-          lo=mid+1;
-        }
-       }
-        else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])  // start from here basic approch
-        {
-            return mid;
-        }
-        else if(nums[mid]<nums[mid+1])
-        {
-            lo=mid+1;
-        }
-        else
-        {
-            hi=mid-1;
-        }
+{
+    int n = nums.size();
+    int lo = 0;
+    int hi = nums.size() - 1;
 
-      }
-      return -1;    
+    if(n == 1) return 0; // ek element hai toh wahi peak hai
+
+    while(lo <= hi)
+    {
+        int mid = lo + (hi - lo) / 2;
+
+        if(mid == 0)   // agar first element par ho
+        {
+            if(nums[mid] > nums[mid+1])
+            {
+                return mid;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+        else if(mid == n-1)  // agar last element par ho
+        {
+            if(nums[mid] > nums[mid-1])
+            {
+                return mid;
+            }
+            else
+            {
+                hi = mid - 1;
+            }
+        }
+        else if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])  // basic approach
+        {
+            return mid;
+        }
+        else if(nums[mid] < nums[mid+1])
+        {
+            lo = mid + 1;
+        }
+        else
+        {
+            hi = mid - 1;
+        }
     }
+    return -1;    
+}
 };
