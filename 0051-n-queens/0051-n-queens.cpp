facbe958306d9b3016282a99e04d498c012e8e04
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-09-24 17:28:12 +0530

    Time: 0 ms (100%), Space: 10.5 MB (38.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,67 +1,66 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
+vector<vector<string>>fans;
 
-    bool isSafe(vector<string> &board,int row,int col,int n)
+bool isSafe(vector<string> &board,int row,int col,int n)
+{
+    // giving i nad j alg alg coz we cantd ierctly manipuldate the row and colm 
+    int i=row-1;
+    int j=col;
+    // cjecking the overgread is quen is present in upper colm return false
+    while(i>=0)
     {
-        // ab 3 condition spr it will be checked 
-
-        // 1st upper colms m nhi hona chaiye koi queen 
-        int i=row-1,j=col;
-        while(i>=0)
-        {
-            if(board[i][j]=='Q') return false;
-            i--;
-        }
-
-        // left diagonal m nhi hona chaiye koi queen 
-        i=row-1,j=col-1;
-        while(i>=0 && j>=0)
-        {
-            if(board[i][j]=='Q') return false;
-            i--;
-            j--;
-        }
-
-        // right diagonal m nhi hona chaiye koi queen 
-        i=row-1,j=col+1;
-        while(i>=0 && j<n)
-        {
-            if(board[i][j]=='Q') return false;
-            i--;
-            j++;
-        }
-
-        return true;
+        if(board[i][j]=='Q')return false;
+        i--;
     }
-
-    void solve(vector<string> &board,int row,int n)
+    // checaking left diognal queen nhi honi chaiye 
+    i=row-1;
+    j=col-1;
+    while(i>=0 && j>=0)
     {
-        if(row==n)
-        {
-            ans.push_back(board);
-            return ;
-        }
+        if(board[i][j]=='Q')return false;
+        i--;
+        j--;
+    }
+    //checking left digonal quuen nhi honi chaiye 
+    i=row-1;
+    j=col+1;
+    while(i>=0 && j<n)
+    {
+        if(board[i][j]=='Q')return false;
+        i--;
+        j++;
+    }
+    return true ;
+}
 
-        // we will come and place q at every colmn and check whter it is safe or not 
-        for(int col=0;col<n;col++)
+void solve(vector<string> &board,int row,int n)
+{
+    if(row==n)  // abse case ki last row main reach ho gya to ise vector main dalo
+    {
+        fans.push_back(board);
+        return ;
+    }
+    // now we r going to traverse and place our queen in each colm and test it whterr it is possible it to place 
+    // considering rules of chess baord ie no queen should be present overhead, left diognal and right diognal 
+    for(int col=0;col<n;col++)
+    {
+        if(isSafe(board,row,col,n)==true)
         {
-            if(isSafe(board,row,col,n)==true)
-            {
-                board[row][col]='Q';
-                solve(board,row+1,n);
+            // to bhiya is row col pr queen baitha do 
+            board[row][col]='Q';
+            solve(board,row+1,n);
 
-                // backtarck kre to wapas unmark kr do dot 
-                board[row][col]='.';
-            }
+            // when we cover all row and find false we backtrack 
+             board[row][col]='.';
         }
     }
-
+}
     vector<vector<string>> solveNQueens(int n) 
     {
-        // craeting board of char first 
-        vector<string> board(n, string(n, '.')); 
+        // constructing the board 
+        vector<string>board(n,string(n,'.'));
         solve(board,0,n);
-        return ans;
+        return fans;
     }
 };
