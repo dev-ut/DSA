cee3849b9af6ca623e21faf36c3bb28cc72efc27
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-02-19 15:24:37 +0530

    Time: 467 ms (22.81%), Space: 45.4 MB (68.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,34 +2,7 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        // this slinding window approch is not valid if negative lemnts are presnt as ki hmen smjh nhi ayega ki kab window bdhana hai kab ghatna hai  
-    //    int n=nums.size();
-    //    int sum=0;
-    //    int i=0;
-    //    int j=0;
-    //    int count=0;
-    //    while (j < n) 
-    // {
-    //     sum += nums[j];
-
-    //     while (sum > k && i <= j)   // shrink window jab sum bada ho
-    //     {
-    //         sum -= nums[i];
-    //         i++;
-    //     }
-
-    //     if (sum == k)   // agar window ka sum k ke barabar hai
-    //     {
-    //         count++;
-    //     }
-
-    //     j++;
-    // }
-
-    // return count;
-
-    //isliye we are hgoing to use hasmpa + prefix sum approch 
-        int n= nums.size();
+           int n= nums.size();
         // create prefix array in palce
         for(int i=1;i<n;i++)
         {
