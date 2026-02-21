62169f4603e56de5ca65c9fef3b01804c2b0e949
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-02-21 23:06:44 +0530

    Time: 0 ms (100%), Space: 15.1 MB (49.62%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,41 +13,41 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
     {
-        vector<vector<int>> fans;
-        // now following up with bfs
-        if(root==NULL) return fans;
-        queue<TreeNode*> q;
-        q.push(root);
-        int flag=0;  // to jb bhi hum direction chnge rnege we will mark l->r 0 and r->l 1
+       vector<vector<int>>fans;
+       if(root==NULL) return fans;
+       queue<TreeNode*>q;
+       q.push(root);
+       int flag=0;
 
-        while(q.size()>0)
+       while(q.size()>0)
+       {
+        int size=q.size();
+        vector<int>ans(size);
+
+        for(int i=0;i<size;i++)
         {
-            int size=q.size();
-            vector<int> ans(size);
-            // ab humkoutni baar hi chlna hai jitni nodes hai us level main 
-            for(int i=0;i<size;i++)
-            {
-                TreeNode*temp=q.front();
-                q.pop();
-                // check kro ki left toright hai ay right to left
-                if(flag==0) // left to right
-                {
-                   ans[i]=temp->val;
-                   if(temp->left!=NULL) q.push(temp->left);
-                   if(temp->right!=NULL) q.push(temp->right);
+            TreeNode* x=q.front();
+            q.pop();
 
-                }
-                else
-                {
-                    ans[size-1-i]=temp->val;
-                   if(temp->left!=NULL) q.push(temp->left);
-                   if(temp->right!=NULL) q.push(temp->right);
-                }
+            if(flag==0) // left to right normal 
+            {
+              ans[i]=x->val;
+              if(x->left!=NULL) q.push(x->left);
+              if(x->right!=NULL) q.push(x->right);
+              
             }
-            if(flag==0) flag=1;
-            else flag=0;
-            fans.push_back(ans);
+            else
+            {
+             ans[size-1-i]=x->val;   // i keeep value dynaic
+              if(x->left!=NULL) q.push(x->left);
+              if(x->right!=NULL) q.push(x->right);
+              
+            }
         }
-        return fans;
-    }
+        if(flag==0) flag=1;
+        else flag=0;
+        fans.push_back(ans);
+       }
+       return fans;
+}
 };
