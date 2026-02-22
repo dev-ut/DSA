f67ed684d040a845d1a1ff73df9a4c42f8a266e7
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-02-22 15:18:44 +0530

    Time: 0 ms (100%), Space: 22.2 MB (40.4%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,41 +2,40 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) 
     {
-      int n=asteroids.size();
-      stack<int>st;
+       int n= asteroids.size();
+       stack<int>st;
 
-      for(int i=0;i<n;i++)  //[4,7,1,1,2,-3,-7,17,15,-16]
-      {
-        if(asteroids[i]<0) // ie agr negative elment hai to 
-        {
-            while(st.size()>0 && st.top()>0 && abs(st.top())<abs(asteroids[i])) // we will pop the elements until 
+       for(int i=0;i<n;i++)
+       {
+          if(asteroids[i]<0) // when negative then do anythig
+          {
+            while(st.size()>0 && st.top()>0 && abs(st.top())<abs(asteroids[i])) // smaller hai 
             {
                 st.pop();
             }
-            // if the mangintude of postive nad negative elment are equal pop and get indx+1
-            if(st.size()>0 && st.top()>0 && abs(st.top())==abs(asteroids[i]))
+            if(st.size()>0 && st.top()>0 && abs(st.top())==abs(asteroids[i])) // barabr ho to  
             {
-                st.pop();
-            }
+                st.pop();  // or indx+1 to apne app hi ho jyega
+            }   
             else if(st.size()==0 || st.top()<0)
             {
                 st.push(asteroids[i]);
-            }
-        }
-        else  // if sare posistives elment hai to just push back  
-        {
-           st.push(asteroids[i]);
-        }
-      }
+            }  
+          }
+          else // positiev simply push in stack
+          {
+            st.push(asteroids[i]);
+          }
+       }
 
-      // ab ise vecto mai wapas denge
-       vector<int> ans(st.size());
+       vector<int>ans(st.size()); 
 
-      for(int i=st.size()-1;i>=0;i--)
-      {
+       for(int i=st.size()-1;i>=0;i--)
+       {
         ans[i]=st.top();
         st.pop();
-      }
-      return ans;
+       } 
+
+       return ans; 
     }
 };
