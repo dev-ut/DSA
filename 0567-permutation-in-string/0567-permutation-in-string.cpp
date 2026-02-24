8b6383e6a6ba7b4e6ea9e866c8ea449c71d4d83b
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-02-24 14:12:57 +0530

    Time: 3 ms (66.99%), Space: 10 MB (69.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
-bool checkInclusion(string s1, string s2) 
-{
-    int n1 = s1.size();
-    int n2 = s2.size();
-
-    if(n1 > n2) return false;
-
-    vector<int> v1(26, 0); // s1 ke character frequency store karega
-    vector<int> v2(26, 0); // s2 ke window ke character frequency store karega
-
-    // Pehle s1 ke characters ki frequency ko count karo
-    for(auto it: s1)
+    bool checkInclusion(string s1, string s2) 
     {
-        v1[it - 'a']++;
+        int n1=s1.length();
+        int n2=s2.length();
+        if(n1>n2) return false;
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
+        // fillling v1 
+        for(int i=0;i<n1;i++)
+        {
+            v1[s1[i]-'a']++;
+        }
+
+        // now for v2
+        int i=0;
+        int j=0;
+        while(j<n2)
+        {
+            v2[s2[j]-'a']++;
+            if(n1==j-i+1) // window lenhthsame a gyi to check ki v1 v2 barabr hai ki nhi
+            {
+              if(v1==v2) return true;
+            }
+            if(j-i+1<n1)
+            {
+                j++;
+            }
+            else
+            {
+                v2[s2[i]-'a']--;
+                i++;
+                j++;   //taki window size maintain rhe 
+            }
+        }
+        return false;
     }
-
-    int i = 0, j = 0;
-
-    // Sliding window approach lagao
-    while(j < n2)
-    {
-        // Window me current character ko add karo
-        v2[s2[j] - 'a']++;
-
-        // Agar window size n1 ke barabar ho jaaye
-        if(j - i + 1 == n1)
-        {
-            // Compare karo ki dono vectors same hain ki nahi
-            if(v1 == v2) return true;
-        }
-
-        // Agar window size n1 se chhoti hai, to bas window ko badhao
-        if(j - i + 1 < n1)
-        {
-            j++;
-        }
-        else
-        {
-            // Agar window size n1 ke barabar ho gai hai, aur chracter abhi mila bhi nhi hai ,to piche wale character ko nikalo
-            v2[s2[i] - 'a']--;
-            i++;
-            j++;
-        }
-    }
-
-    return false;
-}
-
 };
