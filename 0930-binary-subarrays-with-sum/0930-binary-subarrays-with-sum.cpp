38ce1072982cbea60469555fd7cabbd8ae779edb
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-03-15 13:40:52 +0530

    Time: 3 ms (56.39%), Space: 32.5 MB (98.22%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-int numSubarraysWithSumAtmostK(vector<int>& nums, int goal)
+int numSubarraysWithSumWithAtmostgoal(vector<int>& nums, int goal)
 {
     int n=nums.size();
     int i=0;
     int j=0;
     int sum=0;
     int count=0;
+
     while(j<n)
     {
-      sum=sum+nums[j];
-      while(sum>goal && i<=j)
-      {
-        sum=sum-nums[i];
-        i++;
-      }
-      count=count+(j-i+1);  // it will give no of the the subarry at most k from every indx 
-      j++;
+        sum=sum+nums[j];
+
+        while(sum>goal && i<=j)
+        {
+            sum=sum-nums[i];
+            i++;
+        }
+
+
+        // and we will count all the no jinka sum goal se chota hi hai kyubki thy will further contribute in goal making 
+        count=count+(j-i+1);
+        j++;
     }
     return count;
 }
-int numSubarraysWithSum(vector<int>& nums, int goal) 
-{
-    return numSubarraysWithSumAtmostK(nums,goal)-numSubarraysWithSumAtmostK(nums,goal-1);
-}
+    int numSubarraysWithSum(vector<int>& nums, int goal) 
+    {
+       // so diffrece btween jinka sum atmost k hai and jinka sum atmost goal-1 hai 
+       // which will resultantly gives us the subarray jinka sum exactlt goal hai .
 
+       return numSubarraysWithSumWithAtmostgoal(nums,goal)-numSubarraysWithSumWithAtmostgoal(nums,goal-1); 
+    }
 };
