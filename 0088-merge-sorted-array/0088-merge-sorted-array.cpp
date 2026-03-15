9cdacc99730c56676cf3364f2285c2fae7c02f9a
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-03-16 02:10:03 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,39 +2,44 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
     {
-        // ok so the length of m is exactlt m+n 
-        // so we will actlayy start the merging from the first 0th elment in nums1
-        int i=0;
-        int j=0;
-       vector<int> temp; // temporary array for sorted result
+        // eak ddora vector bayen and just expolre the meachnisams 
+          vector<int>ans(m+n);
 
-        while(i < m && j < n) 
-        {
-            if(nums1[i] <= nums2[j]) 
+          int i=0;
+          int j=0;
+          int k=0 ; // just for the3rd vector 
+
+          while(i<m && j<n)
+          {
+            if(nums1[i]<=nums2[j])
             {
-                temp.push_back(nums1[i++]);
-            } 
-            else 
-            {
-                temp.push_back(nums2[j++]);
+                ans[k++]=nums1[i++];
             }
-        }
-        // nums1 ke remaining elements
-        while(i < m) 
-        {
-            temp.push_back(nums1[i++]);
-        }
-          // nums2 ke remaining elements
-        while(j < n) 
-        {
-            temp.push_back(nums2[j++]);
-        }
+            else
+            {
+                ans[k++]=nums2[j++];
+            }
+          }
+          // agr nums1 bdi hai 
+          while(i<m)
+          {
+            ans[k++]=nums1[i++];
+          }
 
-        // ab temp ka data nums1 me overwrite karna
-        for(int k = 0; k < m + n; k++) 
-        {
-            nums1[k] = temp[k];
-        }
-       
+          // agr nums2 me elments bach gaye hain 
+          while(j<n)
+          {
+            ans[k++]=nums2[j++];
+          }
+
+          // ab ans ko sare elmnets nums1 me daal do or just return ans 
+          for(int i=0;i<ans.size();i++)
+          {
+            nums1[i]= ans[i];
+          }
+
+
+
+
     }
 };
