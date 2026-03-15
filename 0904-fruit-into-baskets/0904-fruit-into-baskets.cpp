407306b4fd3fdfea3c913074afa487b16d832856
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-03-15 22:20:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,27 +5,25 @@ public:
         int n =fruits.size();
         int i=0;
         int j=0;
-        int mxlength=0;
         unordered_map<int,int>mp;
+        int mxlength=0;
         while(j<n)
         {
-         mp[fruits[j]]++;
-         if(mp.size()>2)
-         {
-           while(mp.size()>2 && i<=j)
-           {
-            mp[fruits[i]]--;
-            if(mp[fruits[i]]==0)
+            mp[fruits[j]]++;
+            // agr elmnts in basket Ie: map main agr exceed krte hain to just shrink the window
+            if(mp.size()>2)
             {
-                mp.erase(fruits[i]);
-            }
-            i++; // shrinking the window
-           }
-         }
-         mxlength=max(mxlength,j-i+1);
+                while(mp.size()>2 && i<=j)
+                {
 
-         j++;
+                    mp.erase(fruits[i]);
+                    i++;
+                }
+            }
+            mxlength=max(mxlength,j-i+1);
+           j++;
         }
         return mxlength;
     }
 };
+// note : we just have to give the count s see the 3 example we are just returning the window 2 basket can hold . and try to act map like a basket.
