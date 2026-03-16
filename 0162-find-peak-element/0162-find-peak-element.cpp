8a227ad7f353e32caeaeb3833a79f6080e2714d4
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-03-16 15:27:13 +0530

    Time: 0 ms (100%), Space: 12.4 MB (93.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) 
-{
-    int n = nums.size();
-    int lo = 0;
-    int hi = nums.size() - 1;
-
-    if(n == 1) return 0; // ek element hai toh wahi peak hai
-
-    while(lo <= hi)
     {
-        int mid = lo + (hi - lo) / 2;
+        int n=nums.size();
+        int lo=0;
+        int hi=n-1;
+        if(n==1) return 0;
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+           if(mid==0)
+           {
+            if(nums[mid]>nums[mid+1])
+            {
+                return mid;
+            }
+            else
+            {
+                lo=mid+1;
+            }
+           }
+           else if(mid==n-1)
+           {
+            if(nums[mid]>nums[mid-1])
+            {
+                return mid;
+            }
+            else
+            {
+                hi=mid-1;
+            }
+           }
 
-        if(mid == 0)   // agar first element par ho
-        {
-            if(nums[mid] > nums[mid+1])
+           else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
             {
                 return mid;
             }
-            else
+            else if(nums[mid]<nums[mid+1])
             {
-                lo = mid + 1;
-            }
-        }
-        else if(mid == n-1)  // agar last element par ho
-        {
-            if(nums[mid] > nums[mid-1])
-            {
-                return mid;
+                lo=mid+1;
             }
             else
             {
-                hi = mid - 1;
+                hi=mid-1;
             }
         }
-        else if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])  // basic approach
-        {
-            return mid;
-        }
-        else if(nums[mid] < nums[mid+1])
-        {
-            lo = mid + 1;
-        }
-        else
-        {
-            hi = mid - 1;
-        }
+      return -1;
     }
-    return -1;    
-}
 };
