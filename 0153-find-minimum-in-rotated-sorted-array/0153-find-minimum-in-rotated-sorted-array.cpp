9bc161b945904a8b8075d687ace48927f05d675b
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-03-16 14:31:58 +0530

    Time: 0 ms (100%), Space: 14.1 MB (49.21%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,31 +1,29 @@
- // take 2 arrays whensover u have to find minimum in  roated bs 
-    //1,2,3,4,,5,6,7,8,9 
-    //9,8,7,6,5,4,3,2,1     .. one sorted in ascending order ... sorted in descending order 
 class Solution {
 public:
     int findMin(vector<int>& nums) 
     {
-        int n=nums.size();
-
-
-        int lo=0;
-        int hi=n-1;
-        int ans=INT_MAX;
-        while(lo<=hi)
+      int n=nums.size();
+      int lo=0;
+      int hi=n-1;
+      int ans=INT_MAX;
+      while(lo<=hi)
+      {
+        int mid=lo+(hi-lo)/2;
+        //left sorted part check for min 
+        if(nums[lo]<=nums[mid])
         {
-            int mid= lo+(hi-lo)/2;
-            if(nums[lo]<=nums[mid])   // it mans right is ssorted
-            {
-                ans=min(ans,nums[lo]);
-                lo=mid+1;
-            }
-            else
-            {
-                hi=mid-1;
-                ans=min(ans,nums[mid]);  // {3,1,2}
-            }
+            ans=min(ans,nums[lo]);
+            lo=mid+1;               // isse na hmara rigt part acess ho ja rha hai asani se 
         }
-        return ans;
+        else
+        {
+            hi=mid-1;
+           ans=min(ans,nums[mid]);
+           
+        }
 
+      }
+      return ans;    
     }
 };
+//Note : expmaple first wala hi rkho {3,4,5,1,2};
