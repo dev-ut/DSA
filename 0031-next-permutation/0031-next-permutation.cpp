62d91ba2b4947fd82818cac4de2dc304229e54b3
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-04-02 22:59:52 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,38 +2,37 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) 
     {
-        int n=nums.size();
         int pindx=-1;
-        //{1,3,2,4}
+        int n=nums.size();
+        // find the the no from theleft which is just smaller than the previous no '
+        for(int i=n-2;i>=0;i--)
+        {
+           if(nums[i]<nums[i+1])
+           {
+            pindx=i;
+            break;
+           }
+        }
 
-        for(int i=n-2;i>=0;i--)  // we are going to fnd the no whic is going to be just smaller then the no at rhe right
+        if(pindx==-1)  // if not found just revrse for case 2
         {
-            if(nums[i]<nums[i+1])
-            { 
-                pindx=i;
-                break;
-            }
+            reverse(nums.begin(),nums.end());
+            return;
         }
-        if(pindx==-1)   // in this cause the array is [3,2,1]  this poora ulta
-        {
-          reverse(nums.begin(),nums.end());
-          return;
-        }
-        // now second step is to reverse the array with pindx+1
+        // reverse the ntire partafter pindx
         reverse(nums.begin()+pindx+1,nums.end());
-    
-        // third step is to swap the no whic is just greater then the pindx value 
+
+        // find the no just greater than the pindx to the right 
 
         for(int i=pindx+1;i<n;i++)
         {
-         if(nums[i]>nums[pindx])
-         {
-            swap(nums[i],nums[pindx]);
-            break;
-         }
-        }
+            if(nums[pindx]<nums[i])
+            {
+                swap(nums[pindx],nums[i]);
+                break;
+            }
+        } 
+
         return ;
-
-
     }
 };
