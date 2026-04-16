e7de17c96d8244b2e7f0342bedf9950cc08c889a
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-04-16 19:53:00 +0530

    Time: 0 ms (100%), Space: 21.5 MB (66.91%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,24 +1,31 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-bool preorder(TreeNode* root, int targetSum)
-{
-    if(root == NULL) return false;
-    if(root->left == NULL && root->right == NULL) 
-    {
-         if (root->val == targetSum) return true;
-         else return false;
-    }
-     
-    // Traverse left and right subtrees
-    bool leftPath = preorder(root->left, targetSum - root->val);
-    bool rightPath = preorder(root->right, targetSum - root->val);   // hmne yahan yeh variable isliye bnaya hai taki hamare paas ek record rhe path ka
-
-    // Return true if either left or right subtree has a path sum equal to targetSum
-    return leftPath || rightPath;
-}
     bool hasPathSum(TreeNode* root, int targetSum) 
     {
-        return preorder(root, targetSum);
+       if(root==NULL) return false;
+       // if its a chile leaft
+       if(root->left==NULL && root->right==NULL)
+       {
+        if(root->val==targetSum) return true;
+        else return false;
+       }
+       bool leftpath= hasPathSum(root->left,targetSum-root->val);
+       bool rightpath= hasPathSum(root->right,targetSum-root->val);
+
+       if(leftpath==true) return true;
+       if(rightpath==true) return true;
+
+       return false;   
     }
 };
