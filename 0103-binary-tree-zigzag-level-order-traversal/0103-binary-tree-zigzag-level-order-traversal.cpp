3eba162fbaf8194f2969190cad350157bf4f7a8f
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-04-17 14:47:36 +0530

    Time: 0 ms (100%), Space: 15.1 MB (77.22%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,41 +13,39 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
     {
-       vector<vector<int>>fans;
-       if(root==NULL) return fans;
-       queue<TreeNode*>q;
-       q.push(root);
-       int flag=0;
+      //lot
+      vector<vector<int>>fans;
+      queue<TreeNode*>q;
+      if(root==NULL) return fans;
+      q.push(root);
+      int flag=0;
+      while(q.size()>0)
+      {
+        int s=q.size();
+        vector<int>ans(s);
 
-       while(q.size()>0)
-       {
-        int size=q.size();
-        vector<int>ans(size);
-
-        for(int i=0;i<size;i++)
+        for(int i=0;i<s;i++)
         {
             TreeNode* x=q.front();
             q.pop();
 
-            if(flag==0) // left to right normal 
-            {
-              ans[i]=x->val;
-              if(x->left!=NULL) q.push(x->left);
-              if(x->right!=NULL) q.push(x->right);
-              
-            }
-            else
-            {
-             ans[size-1-i]=x->val;   // i keeep value dynaic
-              if(x->left!=NULL) q.push(x->left);
-              if(x->right!=NULL) q.push(x->right);
-              
-            }
+           if(flag==0)
+           {
+            ans[i]=x->val;
+            if(x->left!=NULL) q.push(x->left);
+            if(x->right!=NULL)q.push(x->right);
+           }
+           else
+           {
+            ans[s-i-1]=x->val;
+            if(x->left!=NULL) q.push(x->left);
+            if(x->right!=NULL)q.push(x->right);
+           }
         }
         if(flag==0) flag=1;
-        else flag=0;
+         else flag=0;  
         fans.push_back(ans);
-       }
-       return fans;
-}
+      }  
+      return fans;
+    }
 };
