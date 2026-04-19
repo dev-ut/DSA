14d368103bea6b1b0a4a42c7bee6cb59886de529
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-04-19 09:25:19 +0530

    Time: 0 ms (100%), Space: 77.2 MB (12.11%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,39 +12,38 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
-        ListNode*tempA=l1;
-        ListNode*tempB=l2;
-        //wee willll create the another liat in which sum will be stored
-        ListNode* c= new ListNode(-1);
-        ListNode* tempc=c;               // pointer to the new list
-        int carry =0;
-        while(tempA!=NULL || tempB!=NULL)
-        {
-           
-            int sum=0+carry;
-           if(tempA!=NULL)
-           {
-               sum=sum+tempA->val;
-               tempA=tempA->next;
-           }
-            if(tempB!=NULL)
-           {
-               sum=sum+tempB->val;
-               tempB=tempB->next;
-           }
-           //now look if tehres any carry and u have to fill the last digit of the no so here we will going to use mod and devide
+      ListNode* t1=l1;
+      ListNode* t2=l2;
+      ListNode* c=new ListNode(0);
+      ListNode* tempc=c;
+      int carry=0;
+      while(t1!=NULL || t2!=NULL)
+      {
+        int sum=0+carry;
 
-           carry=sum/10;
-           sum=sum%10;
-           // now create new node
-           tempc->next=new ListNode(sum);
-           tempc=tempc->next;
+        if(t1!=NULL)
+        {
+            sum+=t1->val;
+            t1=t1->next;
         }
-         if (carry > 0) 
-         {
-           tempc->next = new ListNode(carry);
-         }
-       tempc=c;
-        return tempc->next;
+        if(t2!=NULL)
+        {
+            sum+=t2->val;
+            t2=t2->next;
+        }
+
+        carry=sum/10;
+        sum=sum%10;
+        tempc->next= new ListNode (sum);
+        tempc=tempc->next;
+
+      } 
+
+      if(carry>0)
+      {
+        tempc->next= new ListNode (carry);
+      }
+      tempc=c;
+      return tempc->next;   
     }
 };
