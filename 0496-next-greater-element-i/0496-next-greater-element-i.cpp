dbc55f69298129d80a602e1833375f9e7c2b3987
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-04-25 21:42:14 +0530

    Time: 0 ms (100%), Space: 12.8 MB (59.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,47 +2,34 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
-      int n1=nums1.size();
-      int n2=nums2.size();
-
-      // finfing nge
-      vector<int>nge(n2,-1);
-      stack<int>st;
-      st.push(nums2[n2-1]);
-       
-      for(int i=n2-2;i>=0;i--)
+       int n1=nums1.size();
+       int n2=nums2.size();
+       stack<int>st;
+       vector<int>help(n2);
+    for(int i=n2-1;i>=0;i--)
+    {
+      while(st.size()>0 && st.top()<=nums2[i])
       {
-        while(st.size()>0 && st.top()<=nums2[i])
-        {
-            st.pop();
-        }
-        if(st.size()>0) nge[i]=st.top();
-    
-
-        st.push(nums2[i]);
+        st.pop();
       }
-      for(int i=0;i<nge.size();i++)
-      {
-        cout<<nge[i]<<",";
-      }
+      if(st.size()==0) help[i]=-1;
+      else help[i]=st.top();
 
-      // now map the nums2 elemnts with nge
-      unordered_map<int,int>mp;
+      st.push(nums2[i]);
+    }
 
-      for(int i=0;i<nge.size();i++)
-      {
-        mp[nums2[i]]=nge[i];
-      }
-      // now match the nums1 elment in map if got mainatainsn vector push the respected value ie nge for it 
-      vector<int>ans;
-
-     for(int i=0; i<n1; i++) 
-     {
-        ans.push_back(mp[nums1[i]]);
-     }
-
-
-      return ans;
-
+    // now map it 
+    vector<int>ans;
+    unordered_map<int,int>mp;
+    for(int i=0;i<n2;i++)
+    {
+        mp[nums2[i]]=help[i];
+    }
+    // we will just build ans 
+    for(int i=0;i<n1;i++)
+    {
+        ans.push_back(mp[nums1[i]]);   // it will give respective value for this key 
+    }
+        return ans;   
     }
 };
