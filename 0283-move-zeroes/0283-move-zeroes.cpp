df7c6ae2271ed78bd1c10668d9f2a0366ac24b82
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-05-16 18:34:05 +0530

    Time: 0 ms (100%), Space: 24 MB (18.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-void moveZeroes(vector<int>& nums)
-{
-    int n = nums.size();
-    int i = 0; // slow pointer: next position to place non-zero
-
-    // Step 1: Move all non-zero elements to front
-    for (int j = 0; j < n; j++)
+    void moveZeroes(vector<int>& nums) 
     {
-        if (nums[j] != 0)
+        int indx=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            nums[i] = nums[j];
-            i++;
+          if(nums[i]!=0)
+          {
+            nums[indx++]=nums[i];
+          }
         }
-    }
-    // Step 2: Fill remaining positions with 0
-    while (i < n)
-    {
-        nums[i] = 0;
-        i++;
-    }
-}
 
+        for(int i=indx;i<n;i++)
+        {
+            nums[i]=0;
+        }
+        return ;
+    }
 };
