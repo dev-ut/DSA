a0608082604f47e1b45a81097181bbd8c59a2b3b
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-05-17 15:37:51 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) 
     {
-        int pindx=-1;
-        int n=nums.size();
-        // find the the no from theleft which is just smaller than the previous no '
-        for(int i=n-2;i>=0;i--)
+      int n=nums.size();
+      int pindx=-1;
+      // find the  first no which is just smaller from the backside 
+      for(int i=n-2;i>=0;i--)
+      {
+        if(nums[i]<nums[i+1])
         {
-           if(nums[i]<nums[i+1])
-           {
             pindx=i;
             break;
-           }
         }
+      }
+      if(pindx==-1) // sorted order
+      {
+        reverse(nums.begin(),nums.end());
+        return;
+      }
+      // if we got pindx revrse the array from pindx till end
+      reverse(nums.begin()+pindx+1,nums.end());
 
-        if(pindx==-1)  // if not found just revrse for case 2
+      // just find the first greatest no from the pindx
+      for(int i=pindx+1;i<n;i++) 
+      {
+        if(nums[pindx]<nums[i])
         {
-            reverse(nums.begin(),nums.end());
-            return;
+            swap(nums[i],nums[pindx]);
+            break;
         }
-        // reverse the ntire partafter pindx
-        reverse(nums.begin()+pindx+1,nums.end());
-
-        // find the no just greater than the pindx to the right 
-
-        for(int i=pindx+1;i<n;i++)
-        {
-            if(nums[pindx]<nums[i])
-            {
-                swap(nums[pindx],nums[i]);
-                break;
-            }
-        } 
-
-        return ;
+      }    
     }
 };
