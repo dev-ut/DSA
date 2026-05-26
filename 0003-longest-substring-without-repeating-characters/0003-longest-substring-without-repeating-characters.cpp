c2524c89a3e1f2381b7e45dd4b29ca6277ca75bf
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-05-26 11:44:13 +0530

    Time: 17 ms (31.52%), Space: 14.4 MB (20.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,23 +5,25 @@ public:
         int n=s.length();
         int i=0;
         int j=0;
-        int mxlgth=0;
-        unordered_set<char>st;
+        int mws=0;
+        unordered_set<int>st;
 
         while(j<n)
         {
-            if(st.find(s[j])==st.end())
-            {
-                st.insert(s[j]);
-                mxlgth=max(mxlgth,j-i+1);
-                j++;
-            }
-            else
+
+            if(st.find(s[j])!=st.end())
             {
                 st.erase(s[i]);
                 i++;
             }
+            else
+            {
+                st.insert(s[j]);
+                mws=max(mws,j-i+1);
+                j++;
+            }
+         
         }
-        return mxlgth;
+        return mws;
     }
 };
