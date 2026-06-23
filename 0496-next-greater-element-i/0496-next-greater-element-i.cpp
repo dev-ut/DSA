81ce8a7da6a7aca644c005c3f2aee38d19202216
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-06-23 23:31:37 +0530

    Time: 0 ms (100%), Space: 13 MB (15.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
     {
-       int n1=nums1.size();
-       int n2=nums2.size();
-       stack<int>st;
-       vector<int>help(n2);
-    for(int i=n2-1;i>=0;i--)
-    {
-      while(st.size()>0 && st.top()<=nums2[i])
+      int n1=nums1.size();
+      int n2=nums2.size();
+      stack<int>st;
+      vector<int>pge(n2);
+      for(int i=n2-1;i>=0;i--)
       {
-        st.pop();
+        while(st.size()>0 && st.top()<nums2[i])
+        {
+            st.pop();
+        }
+        if(st.size()==0) pge[i]=-1;
+        else pge[i]=st.top();
+
+        st.push(nums2[i]);
+      }  
+      unordered_map<int,int>mp;
+      for(int i=0;i<n2;i++)
+      {
+        mp[nums2[i]]=pge[i];
+      }  
+      vector<int>fans;
+      for(int i=0;i<n1;i++)
+      {
+         fans.push_back(mp[nums1[i]]);
       }
-      if(st.size()==0) help[i]=-1;
-      else help[i]=st.top();
 
-      st.push(nums2[i]);
-    }
-
-    // now map it 
-    vector<int>ans;
-    unordered_map<int,int>mp;
-    for(int i=0;i<n2;i++)
-    {
-        mp[nums2[i]]=help[i];
-    }
-    // we will just build ans 
-    for(int i=0;i<n1;i++)
-    {
-        ans.push_back(mp[nums1[i]]);   // it will give respective value for this key 
-    }
-        return ans;   
+      return fans;
     }
 };
